136589ca85e01f65a48cc27933586e4019e71eac
engmsilva <eng.msilva@yahoo.com.br>
2024-11-22 15:19:07 -0300

    teste de commit

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//comentário para fazer commit
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
